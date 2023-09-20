6720708aceebe02313238c5c25e14cb276f801bf
Thilo1 <thilo.fuehrmann@gmx.net>
2023-09-20 23:24:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-		<p>It's a me, Atom-Mario!</p>
+		<p>Gute Nacht, mein liebes Babechen ❤️ </p>
         <a
           className="App-link"
           href="https://reactjs.org"
