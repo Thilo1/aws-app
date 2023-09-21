3a60c3d88f036d7cd92148d2ead590c8cba2554a
Thilo Führmann <thilo.fuehrmann@gmx.net>
2023-09-21 08:38:44 +0200

    guten morgen update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-		<p>Gute Nacht, mein liebes Babechen ❤️ </p>
+		<p>Guten Morgen, mein liebes Babechen ❤️ </p>
         <a
           className="App-link"
           href="https://reactjs.org"
